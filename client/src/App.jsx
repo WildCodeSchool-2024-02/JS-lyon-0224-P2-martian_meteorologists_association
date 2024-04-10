181134025d88d103b6247e759805984afaaf824d
rostom44 <blackgamerdv7@gmail.com>
2024-04-10 21:10:21 +0200

    add currunt weather and fixed eslint problems

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import WeatherComponent from "./API/Api";
-import React, { useState, useEffect } from 'react';
+import ActualWeather from "./components/ActualWeather/ActualWeather";
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   return (
     <>
-     <WeatherComponent weatherData={weatherData} setWeatherData={setWeatherData} />
-    {weatherData ? (
+      <WeatherComponent
+        weatherData={weatherData}
+        setWeatherData={setWeatherData}
+      />
+      {weatherData ? (
         <div>
+          <ActualWeather
+            currentTemperature={weatherData.current.temperature_2m}
+          />
         </div>
       ) : (
         <p>Loading...</p>
       )}
     </>
-   
   );
 }
 
