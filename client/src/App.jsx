a084d3d391e8674737e8374c535e7a58dab1abdb
Maksim Losjuk <m.lysuk@gmail.com>
2024-04-10 12:31:15 +0200

    Created two components of weather condition and sun position on Mars. Created design for them with CSS and called it to a principal component App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+
 import WeatherComponent from "./API/Api";
+import WeatherContainer from "./components/WeatherContainer/WeatherContainer";
 
 function App() {
   return (
     <>
-    
+      <div>
+        <WeatherContainer />
+      </div>
     </>
-   
+
   );
 }
 
