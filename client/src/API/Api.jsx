c9b8465bc779940af6a6233eb8e6a0c7f132e501
rostom44 <blackgamerdv7@gmail.com>
2024-04-11 10:19:14 +0200

    updated the api

--- client/src/API/Api.jsx
+++ client/src/API/Api.jsx
@@ -1,22 +1,21 @@
 import { useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const WeatherComponent = ({ setWeatherData }) => {
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.open-meteo.com/v1/meteofrance?latitude=45.7485&longitude=4.8467&current=temperature_2m,relative_humidity_2m,is_day,snowfall,cloud_cover,wind_speed_10m&hourly=is_day,sunshine_duration,direct_radiation&daily=sunrise,sunset&timezone=Europe%2FBerlin');
+        const response = await axios.get(
+          "https://api.open-meteo.com/v1/meteofrance?latitude=45.7485&longitude=4.8467&current=temperature_2m,relative_humidity_2m,is_day,snowfall,cloud_cover,wind_speed_10m&hourly=is_day,sunshine_duration,direct_radiation&daily=sunrise,sunset&timezone=Europe%2FBerlin"
+        );
         setWeatherData(response.data);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
     fetchData();
-  }, []);
-
-
+  }, [setWeatherData]);
 };
 
 export default WeatherComponent;
