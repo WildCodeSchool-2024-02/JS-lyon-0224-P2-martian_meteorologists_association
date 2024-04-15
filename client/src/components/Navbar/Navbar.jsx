58ee53694a343d81fb45b3dcbb18da61ef101eeb
rostom44 <blackgamerdv7@gmail.com>
2024-04-15 20:50:28 +0200

    navbar done only css that needs some work for desktop vesrion

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navIcon">
+        <img src="src/assets/images/page2_icon.svg" alt="rocket_icon" />
+      </div>
+    </nav>
+  );
+}
