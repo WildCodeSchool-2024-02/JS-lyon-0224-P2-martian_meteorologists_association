7e3f51a5980d4ae4bbc779b46b36e6f359e19017
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-04-17 09:43:38 +0200

    general HTML and Shipcard css of the reservation page

--- client/src/components/Reservation/Reservation.jsx
+++ client/src/components/Reservation/Reservation.jsx
@@ -6,37 +6,55 @@ export default function Reservation() {
       <h1>choose your shuttle</h1>
       <div className="shipContainer">
         <div className="shipCards">
-          <article>
-            <img src="" alt="" />
-            <h3>0</h3>
+          <article className="shipChoice">
+            <img
+              src="src/assets/images/pexels-pixabay-2159.webp"
+              alt=""
+              width="60px"
+            />
+            <h4>premium shuttle</h4>
           </article>
-          <article>
-            <img src="" alt="" />
-            <h3>0</h3>
+          <article className="shipChoice">
+            <img
+              src="src/assets/images/pexels-pixabay-2159.webp"
+              alt=""
+              width="60px"
+            />
+            <h4>pedalo shuttle</h4>
           </article>
-          <article>
-            <img src="" alt="" />
-            <h3>0</h3>
+          <article className="shipChoice">
+            <img
+              src="src/assets/images/pexels-pixabay-2159.webp"
+              alt=""
+              width="60px"
+            />
+            <h4>medium shuttle</h4>
           </article>
         </div>
+
         <div className="card">
-          <h2>0</h2>
-          <img src="" alt="" />
+          <img
+            src="src/assets/images/pexels-pixabay-2159.webp"
+            alt=""
+            width="90px"
+          />
+          <h2>premium shuttle</h2>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos,
             inventore. Incidunt adipisci natus numquam dolorum aperiam, sequi
             vero, nihil sunt recusandae vitae nobis? Minus aspernatur cum
             exercitationem quia ea quidem!
           </p>
+          <p>price : 500 000 000 $</p>
         </div>
       </div>
+
       <div className="buttons">
         <button type="button">choose your dates</button>
         <button type="button">validate</button>
       </div>
       <div className="price">
-        <h2>price</h2>
-        <p>$</p>
+        <p>500 000 000 $</p>
       </div>
     </div>
   );
