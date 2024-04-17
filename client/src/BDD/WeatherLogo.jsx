0a0dbb4bd51d59d760b1eb6a0e9f7d8dd36c9c36
rostom44 <blackgamerdv7@gmail.com>
2024-04-17 09:18:28 +0200

    small Addition to the BDD

--- client/src/BDD/WeatherLogo.jsx
+++ client/src/BDD/WeatherLogo.jsx
@@ -1,34 +1,34 @@
 const WeatherLogo = [
-    {
-      name: "cloud",
-      imgSrc: "./src/assets/weatherLogo/cloud.svg",
-      activity: "skydiving",
-      clothes: "cloud",
-    },
-    {
-      name: "sun",
-      imgSrc: "./src/assets/weatherLogo/sun.svg",
-      activity: "dont watch the sun",
-      clothes: "sun",
-    },
-    {
-      name: "snow",
-      imgSrc: "./src/assets/weatherLogo/snow.svg",
-      activity: "ski",
-      clothes: "snow",
-    },
-    {
-      name: "storm",
-      imgSrc: "./src/assets/weatherLogo/storm.svg",
-      activity: "kite",
-      clothes: "storm",
-    },
-    {
-      name: "wind",
-      imgSrc: "./src/assets/weatherLogo/wind.svg",
-      activity: "stay home !",
-      clothes: "wind",
-    },
-  ];
-  
-  export default WeatherLogo;
+  {
+    name: "cloud",
+    imgSrc: "./src/assets/weatherLogo/cloud.svg",
+    activity: "time for a small walk",
+    clothes: "Cloudy: Lightweight, UV protection;",
+  },
+  {
+    name: "sun",
+    imgSrc: "./src/assets/weatherLogo/sun.svg",
+    activity: "take the rover out",
+    clothes: "Sunny: Cooling, UV shield",
+  },
+  {
+    name: "snow",
+    imgSrc: "./src/assets/weatherLogo/snow.svg",
+    activity: "play with ice cubes",
+    clothes: "Snowy: Insulated, water-resistant",
+  },
+  {
+    name: "storm",
+    imgSrc: "./src/assets/weatherLogo/storm.svg",
+    activity: "HIDE!!!",
+    clothes: "Stormy: Heavy-duty, full-face helmet.",
+  },
+  {
+    name: "wind",
+    imgSrc: "./src/assets/weatherLogo/wind.svg",
+    activity: "stay home !",
+    clothes: "Windy: Streamlined, windproof.",
+  },
+];
+
+export default WeatherLogo;
