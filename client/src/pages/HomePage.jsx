ce7ea3ed16ae2e77648fdc18a0b08da6e896259e
rostom44 <blackgamerdv7@gmail.com>
2024-04-17 16:32:45 +0200

    corrected a small mistake

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,35 +11,31 @@ function HomePage() {
   const weatherData = useLoaderData("API");
   const [currentWeather, setCurrentWeather] = useState(1);
   return (
-    <>
-      (
-      <div className="mainPage">
-        <div className="weather">
-          <CurrentWeather
-            currentTemperature={weatherData.current.temperature_2m}
+    <div className="mainPage">
+      <div className="weather">
+        <CurrentWeather
+          currentTemperature={weatherData.current.temperature_2m}
+          windSpeed={weatherData.current.wind_speed_10m}
+          opacity={weatherData.current.cloud_cover}
+          snowFall={weatherData.current.snowfall}
+          currentWeather={currentWeather}
+          setCurrentWeather={setCurrentWeather}
+        />
+      </div>
+
+      <div className="globalInformationcontainer">
+        <div className="globalInformation">
+          <WeatherContainer
             windSpeed={weatherData.current.wind_speed_10m}
             opacity={weatherData.current.cloud_cover}
-            snowFall={weatherData.current.snowfall}
-            currentWeather={currentWeather}
-            setCurrentWeather={setCurrentWeather}
+            sunrise={weatherData.daily.sunrise[3]}
+            sunset={weatherData.daily.sunset[3]}
           />
-        </div>
 
-        <div className="globalInformationcontainer">
-          <div className="globalInformation">
-            <WeatherContainer
-              windSpeed={weatherData.current.wind_speed_10m}
-              opacity={weatherData.current.cloud_cover}
-              sunrise={weatherData.daily.sunrise[3]}
-              sunset={weatherData.daily.sunset[3]}
-            />
-
-            <AdditionalInformation CurrentWeather={currentWeather} />
-          </div>
+          <AdditionalInformation CurrentWeather={currentWeather} />
         </div>
       </div>
-      )
-    </>
+    </div>
   );
 }
 
