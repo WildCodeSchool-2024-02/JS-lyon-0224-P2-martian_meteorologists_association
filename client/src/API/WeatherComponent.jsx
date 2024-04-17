63311054c5abb0c266b122d8703c67cc58528cfc
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-04-17 15:47:31 +0200

    Loader in API and currentWeather Comments

--- client/src/API/WeatherComponent.jsx
+++ client/src/API/WeatherComponent.jsx
@@ -1,21 +1,32 @@
-import { useEffect } from "react";
 import axios from "axios";
 
-const fetchData = async ({ setWeatherData }) => {
+// const fetchData = async ({ setWeatherData }) => {
+//   try {
+//     const response = await axios.get(
+//       "https://api.open-meteo.com/v1/meteofrance?latitude=45.7485&longitude=4.8467&current=temperature_2m,relative_humidity_2m,is_day,snowfall,cloud_cover,wind_speed_10m&hourly=is_day,sunshine_duration,direct_radiation&daily=sunrise,sunset&timezone=Europe%2FBerlin"
+//     );
+//     setWeatherData(response.data);
+//   } catch (error) {
+//     console.error("Error fetching weather data:", error);
+//   }
+// };
+
+// const WeatherComponent = ({ setWeatherData }) => {
+//   useEffect(() => {
+//     fetchData(setWeatherData);
+//   }, [setWeatherData]);
+// };
+
+const fetchData = async () => {
   try {
     const response = await axios.get(
       "https://api.open-meteo.com/v1/meteofrance?latitude=45.7485&longitude=4.8467&current=temperature_2m,relative_humidity_2m,is_day,snowfall,cloud_cover,wind_speed_10m&hourly=is_day,sunshine_duration,direct_radiation&daily=sunrise,sunset&timezone=Europe%2FBerlin"
     );
-    setWeatherData(response.data);
+    return response.data;
   } catch (error) {
     console.error("Error fetching weather data:", error);
+    throw error;
   }
 };
 
-const WeatherComponent = ({ setWeatherData }) => {
-  useEffect(() => {
-    fetchData(setWeatherData);
-  }, [setWeatherData]);
-};
-
-export default WeatherComponent;
+export default fetchData;
