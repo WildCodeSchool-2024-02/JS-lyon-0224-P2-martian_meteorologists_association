b9dec27ef8cdd93aeb29446ae50f3516c43e216c
Maksim Losjuk <m.lysuk@gmail.com>
2024-04-18 10:12:46 +0200

    fixed corrections

--- client/src/BDD/ShuttleCards.jsx
+++ client/src/BDD/ShuttleCards.jsx
@@ -3,21 +3,21 @@ const ShuttleCards = [
     activTy: "Premium Shuttle",
     image: "src/assets/images/pexels-pixabay-2159.webp",
     description:
-      "Premium shuttle, with a pool and airconditioning. You'll be able to relax for  the whole 9 month.",
+      "Premium shuttle, with a pool and airconditioning. You'll be able to relax for the 9 months of your travel.",
     price: "$500 000 000",
   },
   {
     activTy: "Medium Shuttle",
     image: "src/assets/images/pexels-pixabay-2159.webp",
     description:
-      "Just stay on your seats for the whole trip, an okay trip that you won't hate nor enjoy very much",
+      "Just stay on your seats for the whole trip, an okay trip that you won't hate nor enjoy very much.",
     price: "$450 000 000",
   },
   {
     activTy: "Pedalo Shuttle",
     image: "src/assets/images/pexels-pixabay-2159.webp",
     description:
-      "Pedal your way to Mars ! You'll have amazing legs for your trip on mars. We do not take any responsabilities for your safety on this trip, chosse carefully",
+      "Pedal your way to Mars ! You'll have the perfect legs for your trip on Mars. We do not take any responsability for your safety on this trip, choose carefully",
     price: "$300 000 000",
   },
 ];
