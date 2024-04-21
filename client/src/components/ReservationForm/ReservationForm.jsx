0d0db10d43ae2fb0ff4f281bcb3f1a097777a73b
Maksim Losjuk <m.lysuk@gmail.com>
2024-04-21 18:06:06 +0200

    useState for the input details

--- client/src/components/ReservationForm/ReservationForm.jsx
+++ client/src/components/ReservationForm/ReservationForm.jsx
@@ -1,7 +1,18 @@
+// import { element } from "prop-types";
 import "./ReservationForm.css";
+import { useState } from "react";
 // import ShuttleCards from "../../BDD/ShuttleCards";
 
 export default function ReservationForm() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const inputDetails = (e) => {
+    e.preventDefault();
+    // console.log({ name, lastName, email });
+  };
+
   return (
     <div>
       <h1>Reservation form</h1>
@@ -10,15 +21,30 @@ export default function ReservationForm() {
           <form id="inputForm">
             <label>
               Name:
-              <input type="text" name="name" />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                name="name"
+              />
             </label>
             <label>
-              Surname:
-              <input type="text" name="surName" />
+              Lastname:
+              <input
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                type="text"
+                name="surName"
+              />
             </label>
             <label>
               Email:
-              <input type="email" name="user_email" />
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                name="user_email"
+              />
             </label>
           </form>
           <div className="chosenShipContainer">
@@ -32,7 +58,7 @@ export default function ReservationForm() {
             <p>price:$500 000 000</p>
           </div>
         </div>
-        <button type="submit" className="validateBtn">
+        <button onClick={inputDetails} type="submit" className="validateBtn">
           Validate
         </button>
       </div>
