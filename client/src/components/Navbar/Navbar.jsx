05d662845b6e36fdafad366d2ee221379182b952
djibmsa@gmail.com <djibmsa@gmail.com>
2024-04-29 11:39:48 +0200

    navbar remix

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,29 +2,28 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
-export default function NavBar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+export default function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowlinks = () => {
+      setShowLinks(!showLinks)
+  }
 
   return (
-    <nav>
-      <button
-        type="button"
-        className="menu-button"
-        onClick={() => {
-          setMenuOpen(!menuOpen);
-        }}
-      >
-        <img src="/src/assets/images/menu_icon.svg" alt="menu button" />
+    <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"} `} >
+      <button type="button"className="navbar_burger" onClick={handleShowlinks}>
+        <span className="burger-bar">merde</span>
       </button>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/">Mars weather</NavLink>
+
+      <ul className="navbar_links">
+      <li className="navbar_item slideInDown-1 ">
+         <NavLink to="/" onClick={handleShowlinks}>Mars weather</NavLink>
         </li>
-        <li>
-          <NavLink to="/reservation">Visit Mars</NavLink>
+        <li className="navbar_item slideInDown-2">
+          <NavLink to="/reservation" onClick={handleShowlinks}>Visit Mars</NavLink>
         </li>
-        <li>
-          <NavLink to="/activities">Activities</NavLink>
+        <li className="navbar_item slideInDown-3">
+          <NavLink to="/activities" onClick={handleShowlinks}>Activities</NavLink>
         </li>
       </ul>
     </nav>
